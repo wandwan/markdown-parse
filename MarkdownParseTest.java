d5efcceaf8dbaf199a18d81961b0bf0dfe1d2905
wandwan <wangandrewwang88@gmail.com>
2022-01-27 20:50:56 -0600

    javac -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,9 +1,9 @@
-import static org.junit.Assert.*;
-import org.junit.*;
+import static org.junit.Assert.*; //import the junit Assert method statically
+import org.junit.*; //import the junit library
 
-public class MarkdownParseTest {
-    @Test
-    public void addition() {
-        assertEquals(2, 1 + 1);
+public class MarkdownParseTest { // create a class called MarkdownParseTest 
+    @Test //Annotation to tell java to test
+    public void addition() { //test method
+        assertEquals(2, 1 + 1); //the test
     }
 }
